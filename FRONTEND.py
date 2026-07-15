acb7335f213b8100d528dd7b59e37554b22823cc
vyshnavimandapuram-coder <vyshnavi.mandapuram@gmail.com>
2026-07-15 10:29:12 +0530

    Update FRONTEND.py

--- FRONTEND.py
+++ FRONTEND.py
@@ -95,8 +95,8 @@ demo = gr.Interface(
 )
 demo.launch()
 
-#if __name__ == "__main__":
-#    demo.launch(
-#        server_name="0.0.0.0",
-#        server_port=int(os.environ.get("PORT", 7860))
-#    )
+if __name__ == "__main__":
+    demo.launch(
+        server_name="0.0.0.0",
+        server_port=int(os.environ.get("PORT", 7860))
+    )
